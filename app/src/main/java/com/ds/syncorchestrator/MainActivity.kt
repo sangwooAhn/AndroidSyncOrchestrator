d8a7584a40f3acfdc10d3dbabcd6151f2904718f
sangwooAhn <asw7793@gmail.com>
2026-02-09 11:14:25 +0900

    Initial setup / activity test
    Initial setup / activity test

---
+++ app/src/main/java/com/ds/syncorchestrator/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.ds.syncorchestrator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ds.syncorchestrator.ui.theme.SyncOrchestratorTheme
+
+class MainActivity : ComponentActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContent {
+			SyncOrchestratorTheme {
+				// A surface container using the 'background' color from the theme
+				Surface(
+					modifier = Modifier.fillMaxSize(),
+					color = MaterialTheme.colorScheme.background
+				) {
+					Greeting("SANG WOO")
+				}
+			}
+		}
+	}
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+	Text(
+		text = "Hello $name!",
+		modifier = modifier
+	)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+	SyncOrchestratorTheme {
+		Greeting("Android")
+	}
+}
